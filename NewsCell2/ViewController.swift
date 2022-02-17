b74cd7d85537295684cbbdbcfe365931a91b3204
osm0nov1 <osmonovrashit@gmail.com>
2022-02-17 23:29:01 +0600

    Initial Commit

---
+++ NewsCell2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsCell2
+//
+//  Created by Rashit Osmonov on 17/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
